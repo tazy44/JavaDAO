c3d5dabe46a7d2defefbe50181fe385a97795b83
Tazy <tazy30@gmail.com>
2016-10-22 21:55:23 +0200

    update 0.1
    Creating the command-line interface

--- src/Main.java
+++ src/Main.java
@@ -101,49 +101,6 @@ public class Main {
                     break;
             }
 
-//            if (process == "insert") {
-//
-//                Product product = new Product(222);
-//                product.setManufacturer("philips");
-//                product.setType("defibrillator");
-//                product.setProductionDate("4/5/2013");
-//                product.setExpiryDate("3/5/2017");
-//                connection.insertProduct(product);
-//
-//            } else if (process == "retrieve") {
-//
-//                List<Product> showResults = new ArrayList<Product>(); //An ArrayList to hold the retrieved data
-//                showResults = connection.retrieveProduct("philips"); //Retrieval of data
-//                System.out.println("ID  Type  Manufacturer  Production_Date  Expiry_Date"); //The order of data printed
-//                int i = 0;
-//                for (i = 0; i < showResults.size(); i++) { //Printing the retrieved data
-//                    System.out.println(showResults.get(i).getId() + " " + showResults.get(i).getType() + " " +
-//                            showResults.get(i).getManufacturer() + "   " + showResults.get(i).getProductionDate() + "   " +
-//                            showResults.get(i).getExpiryDate());
-//                }
-//
-//            } else if (process == "delete") {
-//
-//                connection.deleteProduct(222);
-//
-//            } else if (process == "update") {
-//
-//                Product product = new Product(1212);
-//
-//                product.setManufacturer("philips");
-//                product.setType("x-ray");
-//                product.setProductionDate("4/4/2015");
-//                product.setExpiryDate("3/7/2020");
-//
-////            product.setManufacturer("");
-////            product.setType("incubator");
-////            product.setProductionDate("NULL");
-////            product.setExpiryDate("1/4/2017");
-//                //System.out.println("Almost there!");
-//                connection.updateProduct(product);
-//
-//            } else
-//                System.out.println("Command not recognized!");
         }
     }
 }
